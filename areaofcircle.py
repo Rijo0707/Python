009918d8737c12460c4260e9fcb3c25808813b25
Rijo0707 <77331943+Rijo0707@users.noreply.github.com>
2021-01-12 17:48:50 +0530

    Created areaofcircle.py
    This program calculates the area of the circle.

---
+++ areaofcircle.py
@@ -0,0 +1,5 @@
+#Program to calculate the area of circle
+import math
+r=float(input("Input the radius of the circle: "))
+area=math.pi*r*r
+print("The area of the circle with radius",r,"is: ",area)
